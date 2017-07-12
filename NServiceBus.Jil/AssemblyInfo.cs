0c3b213218667ae95435445ad04962b1a5a874a5
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 13:31:36 +1000

    simplify build

--- NServiceBus.Jil/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NServiceBus.Jil")]
-[assembly: AssemblyProduct("NServiceBus.Jil")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
